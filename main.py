1378569dcff8113628ab7af2522dce0139b807f6
ergunmalay <prokillerhd32@gmail.com>
2020-12-10 15:11:35 +0000

    Kick and Ban Commands added

--- main.py
+++ main.py
@@ -95,5 +95,8 @@ async def on_command_error(context, error):
         await context.message.channel.send(embed=PermsEmbed)
 
 
+
+
+
 ### DON'T REVEAL YOU FUCKING RETARDED MONKEY##
 client.run('Nzg2MjM5MjI2MzA3NDExOTY4.X9DgVw.H_jrH8xz9v50ZdfpVHCvXG0tR68')
