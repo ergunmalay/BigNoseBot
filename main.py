7075050c2c45cf5985cd8383aa5de8a729da28eb
ergunmalay <prokillerhd32@gmail.com>
2020-12-10 14:42:32 +0000

    Kick and ban commands created Next -Unban -Mute -Unmute

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 import discord
 import datetime
 from discord.ext import commands
-from discord.ext.commands import MissingPermissions
 
 # Client Our Bot
 client = commands.Bot(command_prefix="*",
                       case_insensitive=True)  # case_insensitive=True makes all commands case insensitive :D
 
 
-# Do Stuff
+# Connecting to server message
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -47,29 +46,57 @@ async def kick(context, member: discord.User = None, *, reason="Reason not Speci
         botpfp = client.user.avatar_url
         time = datetime.datetime.utcfromtimestamp(1607549263)
 
-        KickEmbed = discord.Embed(title="Kick Format Format", description="*kick {Member} {Reason}")
+        await context.message.delete(delay=2)
+        KickEmbed = discord.Embed(title="Kick Format Format", description="*kick {Member} {Reason}", color=0x4c00ff)
         KickEmbed.set_thumbnail(url=botpfp)
         KickEmbed.set_footer(text=time)
 
         await context.message.channel.send(embed=KickEmbed)
-
     else:
         userpfp = member.avatar_url
-        await context.guild.kick(member, reason="Reason not provided")
 
-        KickEmbed = discord.Embed(title="User " + member.display_name + " Has been kicked.", description=reason)
+        await context.message.delete(delay=2)
+        KickEmbed = discord.Embed(title="User " + member.display_name + " Has been kicked.", description=reason,
+                                  color=0x4c00ff)
         KickEmbed.set_thumbnail(url=userpfp)
 
         await context.message.channel.send(embed=KickEmbed)
 
 
-@kick.error
-async def kick_error(context, error):
-    if isinstance(error, MissingPermissions):
+# Ban command
+@client.command(name="ban")
+@commands.has_permissions(ban_members=True)
+async def ban(context, member: discord.User = None, *, reason="Reason not Specified"):
+    if member == None:
         botpfp = client.user.avatar_url
+        time = datetime.datetime.utcfromtimestamp(1607549263)
 
-        PermsEmbed = discord.Embed(title= "You do not have the correct permissions.")
+        await context.message.delete(delay=2)
+        BanEmbed = discord.Embed(title="Ban Format Format", description="*ban {Member} {Reason}", color=0x4c00ff)
+        BanEmbed.set_thumbnail(url=botpfp)
+        BanEmbed.set_footer(text=time)
 
+        await context.message.channel.send(embed=BanEmbed)
+    else:
+        userpfp = member.avatar_url
+
+        await context.message.delete(delay=2)
+        BanEmbed = discord.Embed(title="User " + member.display_name + " Has been banned.", description=reason,
+                                 color=0x4c00ff)
+        BanEmbed.set_thumbnail(url=userpfp)
+
+        await context.message.channel.send(embed=BanEmbed)
+
+
+@client.event
+async def on_command_error(context, error):
+    if isinstance(error, commands.MissingPermissions):
+        PermsEmbed = discord.Embed(title="You do not have the correct permissions.", color=0x4c00ff)
+        await context.message.channel.send(embed=PermsEmbed)
+
+    elif isinstance(error, commands.UserNotFound):
+        PermsEmbed = discord.Embed(title="I could not find that member, Make sure to @ the member. \nPlease try again.",
+                                   color=0x4c00ff)
         await context.message.channel.send(embed=PermsEmbed)
 
 
